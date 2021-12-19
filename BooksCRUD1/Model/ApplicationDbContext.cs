322d912015e2cd97494733e1ae28714845fe1bee
DaOptimist <errazkiayoubforever@gmail.com>
2021-12-19 14:56:12 +0100

    setting-up EFn

---
+++ BooksCRUD1/Model/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksCRUD1.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+
+}
